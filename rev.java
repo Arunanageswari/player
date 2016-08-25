4b2cedd064d378fea51c53fb9e96ef580585de46
ARUNANAGESWARI <ablyaruna@gmail.com>
2016-08-25 09:51:01 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String args[]) {
+       Scanner buffer = new Scanner(System.in);
+       String s1=buffer.next();
+       s1.reverse();
+       System.out.println(s1);
+   }  
+}
